8a17bf95f47056872f201e7753c220467513dfc8
Nathan Broadbent <git@ndbroadbent.com>
2021-11-22 13:38:10 +1300

    Fixed calendar names

--- src/index.ts
+++ src/index.ts
@@ -944,9 +944,16 @@ class AtomicCalendarRevive extends LitElement {
 				const calColor = typeof event._config.color != 'undefined' ? event._config.color : this._config.defaultCalColor;
 
 				//show calendar name
-				const eventCalName = event._config.eventCalName
+				let calDisplayName = event._config.eventCalName
+				if (!calDisplayName) {
+					if (event.eventClass && event.eventClass.organizer) {
+						const eventOrganizer = event.eventClass.organizer
+						calDisplayName = eventOrganizer.displayName || eventOrganizer.email
+					}
+				}
+				const eventCalName = calDisplayName
 					? html`<div class="event-cal-name" style="color: ${calColor};">
-							<ha-icon icon="mdi:calendar" class="event-cal-name-icon"></ha-icon>&nbsp;${event._config.eventCalName}
+							<ha-icon icon="mdi:calendar" class="event-cal-name-icon"></ha-icon>&nbsp;${calDisplayName}
 					  </div>`
 					: ``;
 
